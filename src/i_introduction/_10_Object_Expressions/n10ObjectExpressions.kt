0d6d3946440f883a3975192ff5927f6acd52e709
sinlov <sinlovgmppt@gmail.com>
2018-12-26 18:17:54 +0800

    finish task ./gradlew test --tests 'i_*_10_*'

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,7 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+//    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, compareBy<Int>({-it}))
     return arrayList
 }
