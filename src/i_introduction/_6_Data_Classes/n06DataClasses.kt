3f7db1802976f11211a8d7bbbc295fc36110c228
sinlov <sinlovgmppt@gmail.com>
2018-12-26 16:49:50 +0800

    finish task ./gradlew test --tests 'i_*_6_*'

--- src/i_introduction/_6_Data_Classes/n06DataClasses.kt
+++ src/i_introduction/_6_Data_Classes/n06DataClasses.kt
@@ -15,10 +15,14 @@ fun todoTask6(): Nothing = TODO(
     references = { JavaCode6.Person("Alice", 29) }
 )
 
-class Person
+class Person(val name: String, val age: Int){
+    override fun toString(): String {
+        return "Person(name=$name, age=$age)"
+    }
+}
 
 fun task6(): List<Person> {
-    todoTask6()
-    return listOf(/*Person("Alice", 29), Person("Bob", 31)*/)
+//    todoTask6()
+    return listOf(Person("Alice", 29), Person("Bob", 31))
 }
 
